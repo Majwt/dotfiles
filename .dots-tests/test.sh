9c528bd024f79b19bb1dcc460ac521573d617799
Theodor Wase <theodor.wase@icloud.com>
2026-01-27 21:09:43 +0100

    feat(testing): prepare for testing in docker

---
+++ .dots-tests/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+echo "== Testing Arch =="
+docker build -f Dockerfile.arch -t dotfiles-arch ..
+docker run -it dotfiles-arch
+
+echo "== Testing Ubuntu =="
+docker build -f Dockerfile.ubuntu -t dotfiles-ubuntu ..
+docker run -it dotfiles-ubuntu
+
